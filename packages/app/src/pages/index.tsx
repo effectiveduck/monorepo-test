c6d817e5d870b870ba12fe807492100b0897aac5
sraj50 <siddharth.rajesh13@gmail.com>
2021-10-04 15:22:09 +1100

    APIIIIIIIIII in app/index.tsx

--- packages/app/src/pages/index.tsx
+++ packages/app/src/pages/index.tsx
@@ -29,7 +29,8 @@ const Home: NextPage = () => {
           <a href="https://nextjs.org/docs">
             <h2>Documentation &rarr;</h2>
             <p>
-              Find in-depth information about Next.js features and APIIIIIIII.
+              Find in-depth information about Next.js features and
+              APIIIIIIIIIIIIIIIIIIIIIIIII.
             </p>
           </a>
 
